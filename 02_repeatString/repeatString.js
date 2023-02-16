78778fbc1f65e650006d91a12c76a5d41369354e
Andy Molinaro <andrew.molinaro916@gmail.com>
2023-02-15 21:33:14 -0600

    02: completed repeatString example

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR"
+    } else {
+    
+        let res = "";
+        for (let i = 0; i < num; i++) {
+            res = res + str;
+        }
+        return res;
 
+    }
 };
 
 // Do not edit below this line
